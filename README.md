7e0846085fc97bb26b8c70984a1077a4dfcd562c
808 Cleanups <808cleanupsapp@gmail.com>
2024-04-23 12:12:05 -1000

    first commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# MeerkatIO Extension for Visual Studio Code
+
+[MeerkatIO](https://meerkatio.com) is a personal notification tool for software developers with long-running scripts. The MeerkatIO Platform will help you save time and help you to stop watching your code run.
+
+## Features
+
+The MeerkatIO VS Code Extension supports the following notification channels:
+- Ping
+- Slack Direct Message
+- SMS
+- Email
+
+## Quickstart
+
+> Tip: Many popular extensions utilize animations. This is an excellent way to show off your extension! We recommend short, focused animations that are easy to follow.
+
+## Setup
+No setup is required to use the Ping command with this extension, but in order to access Slack, SMS, and Email notification channels a MeerkatIO account is required. [Get started for free today!](https://meerkatio.com/register)
+
+MeerkatIO notifications will automatically (after a window reload) execute for all tasks and debug executions. The communication channel can be quickly changed and notifications can easily be toggled on and off to fit your current task!
+
+## Extension Settings
+After creating your account, set the following Extension Settings to unlock the full MeerkatIO Platform command set:
+
+* `meerkatio.token`: Your MeerkatIO account token which can be found at https://meerkatio.com/account
+* `meerkatio.meerkatNotification`: the notification channel you would like to be alerted with. Available notification options:
+    * ping
+    * slack
+    * email
+    * sms
+* `meerkatio.enabled`: Toggle MeerkatIO notifications on and off
+
+## Release Notes
+
+### 1.1.0
+
+Add MeerkatIO Pro communication channels including Slack, SMS, and Email
+
+### 1.0.0
+
+Initial release of MeerkatIO VSCode Extension
+
+
+**Thank you for your support!**
---
+++ extension.js
@@ -0,0 +1,101 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require('vscode');
+const axios = require('axios');
+var player = require('play-sound')(opts = {});
+//const notifier = require('node-notifier');
+
+function sendMeerkatNotification(method, message) {
+    const url = 'https://meerkatio.com/api/notification/send';
+
+	const token = vscode.workspace.getConfiguration('meerkat').get('token');
+	if (!token) {
+		vscode.window.showErrorMessage("No MeerkatIO token found. Check configuration and try again.");
+	}
+
+    const data = {
+        meerkat_token: token,
+        method,
+		message: "From VS Code: " + message
+    };
+
+    axios.post(url, data, {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then(response => {
+		if (response.status === 200)
+			vscode.window.showInformationMessage(message);
+		else
+			vscode.window.showErrorMessage("MeerkatIO failed to send notification. Please check your credentials and valid configuration options.");
+	})
+	.catch(error => {
+		console.log(error);
+		vscode.window.showErrorMessage('Error contacting MeerkatIO Platform');
+	});
+}
+
+function handleMeerkatNotification(meerkatioNotification, message) {
+	if (meerkatioNotification === 'sound') {
+		player.play(context.extensionPath + '/audio/default_ping.mp3', function(err){
+			if (err) throw err
+			});
+	}
+	else if (meerkatioNotification === 'slack') {
+		console.log("Slack Alert");
+		sendMeerkatNotification('slack', message);
+	}
+	else if (meerkatioNotification === 'sms') {
+		console.log("SMS Alert");
+		sendMeerkatNotification('sms', message);
+	}
+	else if (meerkatioNotification === 'email') {
+		console.log("Email Alert");
+		sendMeerkatNotification('email', message);
+	}
+}
+
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	if (vscode.workspace.getConfiguration('meerkat').get('enabled', true))
+		vscode.window.showInformationMessage('MeerkatIO - your notification manager - is now active!');
+
+	//const debugTaskListener = vscode.debug.onDidEndTask((e) => handleNotification(e, context));
+	const debugTaskListener = vscode.debug.onDidTerminateDebugSession((e) => {
+		if (!vscode.workspace.getConfiguration('meerkat').get('enabled', true))
+			return;
+
+		const meerkatioNotification = vscode.workspace.getConfiguration('meerkat').get('meerkatNotification', 'sound');
+		const message = `Run and Debug Task Completed: ${e.name}`;
+		handleMeerkatNotification(meerkatioNotification, message);
+	})
+
+	const taskListener = vscode.tasks.onDidEndTask((e) => {
+		if (!vscode.workspace.getConfiguration('meerkat').get('enabled', true))
+			return;
+
+		const meerkatioNotification = vscode.workspace.getConfiguration('meerkat').get('meerkatNotification', 'sound');
+		const message = `Task completed: ${e.execution.task.name}`;
+		handleMeerkatNotification(meerkatioNotification, message);
+	});
+
+	context.subscriptions.push(debugTaskListener);
+	context.subscriptions.push(taskListener);
+}
+
+// This method is called when your extension is deactivated
+function deactivate() {}
+
+module.exports = {
+	activate,
+	deactivate
+}
